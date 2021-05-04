9b8f838e0571d51bea3626a61eb7999f0e15e3ce
Aykut Şahin <36954450+aykutsahin98@users.noreply.github.com>
2021-05-04 17:19:12 +0300

    Company Simulation

---
+++ interfaceDemo/src/interfaceDemo/OutSourceWorker.java
@@ -0,0 +1,12 @@
+package interfaceDemo;
+ 
+//firma calisan
+public class OutSourceWorker implements IWorkable{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
